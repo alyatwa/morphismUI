be2fa3f9a7edef55401202c1aa613493683f2e69
ali atwa <ali.atwa2040@gmail.com>
2023-10-07 20:47:55 +0300

    eslint2

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -9,16 +9,11 @@ const Auth = () => {
 		const provider = new GoogleAuthProvider();
 		signInWithPopup(auth, provider)
 			.then((result) => {
-				const credential = GoogleAuthProvider.credentialFromResult(result);
-				const token = credential?.accessToken;
-				const user = result.user;
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+					/* const token = credential?.accessToken;
+				const user = result.user;  */
 			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				const email = error.customData.email;
-				const credential = GoogleAuthProvider.credentialFromError(error); 
-			});
+			
 	};
 	return (
 		<div>
